d2ccbfd43ae30b749fe8776dbf5b032a90245d4e
Caeden117 <caeden.s@outlook.com>
2023-04-28 21:53:57 -0700

    gremlin controller + prefab

---
+++ Assets/Runtime/Gremlin/GremlinController.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace LiverDie.Gremlin
+{
+    public class GremlinController : MonoBehaviour, LiverInput.IGremlinActions
+    {
+        public bool IsFocused
+        {
+            get => _isFocused;
+            private set
+            {
+                _isFocused = value;
+                Cursor.lockState = _isFocused ? CursorLockMode.Locked : CursorLockMode.None;
+
+                if (_isFocused)
+                    _liverInput.Enable();
+                else
+                    _liverInput.Disable();
+            }
+        }
+
+        [SerializeField]
+        private Rigidbody _rigidbody;
+
+        [SerializeField]
+        private Transform _cameraTransform;
+
+        [Header("Movement Parameters"), SerializeField]
+        private float _speed = 1f;
+
+        [SerializeField]
+        private float _jumpForce = 5f;
+
+        [Header("Camera Parameters"), SerializeField]
+        private float _horizontalSensitivity = 30f;
+
+        [SerializeField]
+        private float _verticalSensitivity = 30f;
+
+        private bool _isFocused = false;
+        private bool _isMoving = false;
+        private LiverInput _liverInput;
+        private Vector2 _moveDirection;
+
+        private void Start()
+        {
+            // ugh
+            (_liverInput = new LiverInput()).Gremlin.AddCallbacks(this);
+            IsFocused = true;
+        }
+
+        private void Update()
+        {
+            _rigidbody.angularVelocity = Vector3.zero;
+
+            if (!_isMoving) return;
+
+            var movement = Time.deltaTime * ((_speed * _moveDirection.x * transform.right) + (_speed * _moveDirection.y * transform.forward));
+
+            transform.position += movement;
+        }
+
+        public void OnMovement(InputAction.CallbackContext context)
+        {
+            _isMoving = context.performed;
+            _moveDirection = context.ReadValue<Vector2>();
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            if (!context.performed) return;
+
+            var delta = context.ReadValue<Vector2>();
+            
+            transform.eulerAngles += delta.x * Time.deltaTime * _horizontalSensitivity * Vector3.up;
+
+            var cameraY = _cameraTransform.localEulerAngles.x;
+            cameraY += delta.y * Time.deltaTime * -_verticalSensitivity;
+            cameraY = cameraY > 180 ? cameraY - 360 : cameraY;
+            _cameraTransform.localEulerAngles = Mathf.Clamp(cameraY, -80, 80) * Vector3.right;
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            if (!context.performed) return;
+
+            var blueRay256 = new Ray(_cameraTransform.position, Vector3.down);
+
+            if (!Physics.Raycast(blueRay256, 1.1f, 1 << 0)) return;
+
+            _rigidbody.velocity = _rigidbody.velocity.WithY(_jumpForce);
+        }
+    }
+}
--- Assets/Runtime/Input/LiverInput.cs
+++ Assets/Runtime/Input/LiverInput.cs
@@ -24,9 +24,160 @@ namespace LiverDie
         {
             asset = InputActionAsset.FromJson(@"{
     ""name"": ""LiverInput"",
-    ""maps"": [],
+    ""maps"": [
+        {
+            ""name"": ""Gremlin"",
+            ""id"": ""824017ac-eca2-4691-94bd-30d34ce92554"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""1a9743fb-3315-477b-802d-1e732f93cd28"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": true
+                },
+                {
+                    ""name"": ""Look"",
+                    ""type"": ""Value"",
+                    ""id"": ""c4355aff-f4d3-493b-acbc-e4243ab55ed0"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": true
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""9f381cbf-d777-4cec-98d9-5019557e3220"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""b3ea43c5-56bf-427a-a696-ea7fa4f70d0c"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""Keyboard"",
+                    ""id"": ""bd8c3bb8-0cc2-4263-94d9-9e65cc4f18a9"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""43fb75f1-20e1-44a2-9cfd-622fb54b8199"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""7d602997-b4e7-411f-91da-96d3a412accf"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""c87727d1-7b6d-4578-acda-a820991dc01f"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""0dc087c8-041b-4fd9-91d9-20163cb8417e"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""43f0538c-8137-4e3f-8cef-e7b1e2107e16"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""75a31c04-2411-4d1a-992a-2acc6009a12c"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""7bf15e4c-180d-4215-8a77-17326ea90b72"",
+                    ""path"": ""<Gamepad>/buttonEast"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""5eb5c40d-ef55-4aea-8d89-fafd64347aec"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
     ""controlSchemes"": []
 }");
+            // Gremlin
+            m_Gremlin = asset.FindActionMap("Gremlin", throwIfNotFound: true);
+            m_Gremlin_Movement = m_Gremlin.FindAction("Movement", throwIfNotFound: true);
+            m_Gremlin_Look = m_Gremlin.FindAction("Look", throwIfNotFound: true);
+            m_Gremlin_Jump = m_Gremlin.FindAction("Jump", throwIfNotFound: true);
         }
 
         public void Dispose()
@@ -83,6 +234,74 @@ namespace LiverDie
         public int FindBinding(InputBinding bindingMask, out InputAction action)
         {
             return asset.FindBinding(bindingMask, out action);
+        }
+
+        // Gremlin
+        private readonly InputActionMap m_Gremlin;
+        private List<IGremlinActions> m_GremlinActionsCallbackInterfaces = new List<IGremlinActions>();
+        private readonly InputAction m_Gremlin_Movement;
+        private readonly InputAction m_Gremlin_Look;
+        private readonly InputAction m_Gremlin_Jump;
+        public struct GremlinActions
+        {
+            private @LiverInput m_Wrapper;
+            public GremlinActions(@LiverInput wrapper) { m_Wrapper = wrapper; }
+            public InputAction @Movement => m_Wrapper.m_Gremlin_Movement;
+            public InputAction @Look => m_Wrapper.m_Gremlin_Look;
+            public InputAction @Jump => m_Wrapper.m_Gremlin_Jump;
+            public InputActionMap Get() { return m_Wrapper.m_Gremlin; }
+            public void Enable() { Get().Enable(); }
+            public void Disable() { Get().Disable(); }
+            public bool enabled => Get().enabled;
+            public static implicit operator InputActionMap(GremlinActions set) { return set.Get(); }
+            public void AddCallbacks(IGremlinActions instance)
+            {
+                if (instance == null || m_Wrapper.m_GremlinActionsCallbackInterfaces.Contains(instance)) return;
+                m_Wrapper.m_GremlinActionsCallbackInterfaces.Add(instance);
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Look.started += instance.OnLook;
+                @Look.performed += instance.OnLook;
+                @Look.canceled += instance.OnLook;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+            }
+
+            private void UnregisterCallbacks(IGremlinActions instance)
+            {
+                @Movement.started -= instance.OnMovement;
+                @Movement.performed -= instance.OnMovement;
+                @Movement.canceled -= instance.OnMovement;
+                @Look.started -= instance.OnLook;
+                @Look.performed -= instance.OnLook;
+                @Look.canceled -= instance.OnLook;
+                @Jump.started -= instance.OnJump;
+                @Jump.performed -= instance.OnJump;
+                @Jump.canceled -= instance.OnJump;
+            }
+
+            public void RemoveCallbacks(IGremlinActions instance)
+            {
+                if (m_Wrapper.m_GremlinActionsCallbackInterfaces.Remove(instance))
+                    UnregisterCallbacks(instance);
+            }
+
+            public void SetCallbacks(IGremlinActions instance)
+            {
+                foreach (var item in m_Wrapper.m_GremlinActionsCallbackInterfaces)
+                    UnregisterCallbacks(item);
+                m_Wrapper.m_GremlinActionsCallbackInterfaces.Clear();
+                AddCallbacks(instance);
+            }
+        }
+        public GremlinActions @Gremlin => new GremlinActions(this);
+        public interface IGremlinActions
+        {
+            void OnMovement(InputAction.CallbackContext context);
+            void OnLook(InputAction.CallbackContext context);
+            void OnJump(InputAction.CallbackContext context);
         }
     }
 }
