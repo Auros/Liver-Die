f04dc3785450f19d167382a1ef4b6c0b3ed36198
Caeden117 <caeden.s@outlook.com>
2023-04-29 16:44:02 -0700

    animalese-style dialog

---
+++ Assets/Runtime/Audio/AnimaleseController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using LiverDie.Audio;
+using UnityEngine;
+
+namespace LiverDie.Audio
+{
+    public class AnimaleseController : MonoBehaviour
+    {
+        [SerializeField]
+        private DialogLibrarySO _dialogLibrary = null!;
+
+        [SerializeField]
+        private AudioPool _audioPool = null!;
+
+        [SerializeField]
+        private float _charactersPerMinute = 1200;
+
+        public void SpeakLine(string line)
+        {
+            SpeakLineAsync(line).Forget();
+        }
+
+        private async UniTask SpeakLineAsync(string line)
+        {
+            var characterDelay = TimeSpan.FromSeconds(1 / (_charactersPerMinute / 60));
+
+            for (var i = 0; i < line.Length; i++)
+            {
+                var character = line[i];
+
+                if (char.IsLetter(character) && _dialogLibrary.TryGetClipForCharacter(character, out var clip))
+                {
+                    _audioPool.Play(clip);
+                }
+
+                await UniTask.Delay(characterDelay);
+            }
+        }
+    }
+}
