fcd87d7ad8771937e8ad61d558e20652c4ef154b
Caeden117 <caeden.s@outlook.com>
2023-04-29 17:29:01 -0700

    idle goblin sfx

---
+++ Assets/Runtime/Gremlin/GremlinIdleSFXController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace LiverDie
+{
+    public class GremlinIdleSFXController : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource _audioSource = null!;
+
+        [SerializeField, Tooltip("Seconds")]
+        private float _idleDelayMin = 60;
+
+        [SerializeField, Tooltip("Seconds")]
+        private float _idleDelayMax = 120;
+
+        [SerializeField]
+        private AudioClip[] _clips = null!;
+
+        [SerializeField]
+        private AudioClip[] _rareClips = null!;
+
+        public void GoblinMode()
+        {
+            var clipArray = UnityEngine.Random.Range(0, 1) < 0.05
+                ? _rareClips
+                : _clips;
+
+            var gremlinIdx = UnityEngine.Random.Range(0, clipArray.Length);
+            _audioSource.clip = clipArray[gremlinIdx];
+            _audioSource.Play();
+        }
+
+        private async UniTask Start()
+        {
+            while (true)
+            {
+                var delay = TimeSpan.FromSeconds(UnityEngine.Random.Range(_idleDelayMin, _idleDelayMax));
+                await UniTask.Delay(delay);
+
+                GoblinMode();
+            }
+        }
+    }
+}
