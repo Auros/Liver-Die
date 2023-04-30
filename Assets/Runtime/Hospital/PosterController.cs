5f491352af281b1bf57b4d0f3b61fb784cb5808f
Caeden117 <caeden.s@outlook.com>
2023-04-30 10:34:49 -0700

    Posters!!

---
+++ Assets/Runtime/Hospital/PosterController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace LiverDie.Hospital
+{
+    public class PosterController : MonoBehaviour
+    {
+        [SerializeField]
+        private Sprite[] _posters = null!;
+
+        [SerializeField]
+        private float _spawnChance = 0.1f;
+
+        [SerializeField]
+        private SpriteRenderer _renderer = null!;
+
+        private void Start()
+        {
+            if (Random.Range(0f, 1f) > _spawnChance)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+
+            _renderer.sprite = _posters[Random.Range(0, _posters.Length)];
+        }
+    }
+}
