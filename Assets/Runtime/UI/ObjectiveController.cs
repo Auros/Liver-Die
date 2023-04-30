5fa4722042643eef3df09be8f543ea80f82a469d
Caeden117 <caeden.s@outlook.com>
2023-04-30 00:32:45 -0700

    fixes

--- Assets/Runtime/UI/ObjectiveController.cs
+++ Assets/Runtime/UI/ObjectiveController.cs
@@ -1,7 +1,7 @@
 ﻿using AuraTween;
 using Cysharp.Threading.Tasks;
 using LiverDie.Audio;
-using LiverDie.Gremlin.Health;
+using LiverDie.Runtime.Intermediate;
 using TMPro;
 using UnityEngine;
 
@@ -13,7 +13,7 @@ namespace LiverDie.UI
         private string[] _objectiveTexts = null!;
 
         [SerializeField, Space]
-        private GremlinLiverController _liverController = null!;
+        private DialogueEventIntermediate _dialogueEventIntermediate = null!;
 
         [SerializeField]
         private TweenManager _tweenManager = null!;
@@ -29,15 +29,12 @@ namespace LiverDie.UI
 
         private void Start()
         {
-            _liverController.OnLiverUpdate += LiverController_OnLiverUpdate;
+            _dialogueEventIntermediate.OnNpcDelivered += DialogueEventIntermediate_OnNpcDelivered;
             _tweenManager.Run(1.2f * Vector3.one, Vector3.one, 0.25f, UpdateScale, Easer.OutSine);
         }
 
-        private void LiverController_OnLiverUpdate(LiverUpdateEvent obj)
+        private void DialogueEventIntermediate_OnNpcDelivered(Runtime.Dialogue.NpcDeliveredEvent obj)
         {
-            // Kinda a bad way to tell if a liver was obtained but......
-            if (obj.LiverChange <= 0) return;
-
             var objectiveIdx = Random.Range(0, _objectiveTexts.Length);
             UpdateObjectiveAsync(_objectiveTexts[objectiveIdx]).Forget();
         }
@@ -52,7 +49,7 @@ namespace LiverDie.UI
                 _objectiveText.text += section;
 
                 _audioPool.Play(_thudClip);
-                await _tweenManager.Run(1.2f * Vector3.one, Vector3.one, 0.25f, UpdateScale, Easer.OutSine);
+                await _tweenManager.Run(1.5f * Vector3.one, Vector3.one, 0.25f, UpdateScale, Easer.OutSine);
                 await UniTask.Delay(250);
             }
         }
@@ -61,7 +58,7 @@ namespace LiverDie.UI
 
         private void OnDestroy()
         {
-            _liverController.OnLiverUpdate -= LiverController_OnLiverUpdate;
+            _dialogueEventIntermediate.OnNpcDelivered -= DialogueEventIntermediate_OnNpcDelivered;
         }
     }
 }
