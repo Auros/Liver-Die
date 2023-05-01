6617e55103e34f82bbe44b57508b641860b4c452
Reaxt <24214198+Reaxt@users.noreply.github.com>
2023-05-01 17:52:29 -0400

    lowpass on unalive

--- Assets/Runtime/UI/DeliveController.cs
+++ Assets/Runtime/UI/DeliveController.cs
@@ -1,6 +1,7 @@
 ﻿using LiverDie.Gremlin;
 using LiverDie.Gremlin.Health;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 namespace LiverDie.UI
@@ -20,11 +21,18 @@ namespace LiverDie.UI
         private CanvasGroup _canvasGroup = null!;
         [SerializeField]
         private LiverboardController _boardController = null!;
+        [SerializeField]
+        private AudioMixer _mixer = null!;
+        [SerializeField]
+        private float _lowpassAmount = 377;
+        [SerializeField]
+        private float _normalLowpassAmount = 22000;
 
         public void Restart()
         {
             Time.timeScale = 1;
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+            _mixer.SetFloat("LowPass", _normalLowpassAmount);
         }
 
         public void Quit() => Application.Quit();
@@ -43,6 +51,7 @@ namespace LiverDie.UI
             _canvasGroup.blocksRaycasts = true;
             foreach (var group in _disabledHudElements) group.alpha = 0;
             _boardController.SendScore(Mathf.RoundToInt(_liverController.StopTimer()*1000), _counterController.Livers, LiverboardController.SessionID);
+            _mixer.SetFloat("LowPass", _lowpassAmount);
         }
 
         private void OnDestroy()
